7609623ab681fb9c681cde81ee87e7af8b635fb4
wprkftlgus <wprkftlgus@naver.com>
2026-01-21 16:40:32 +0900

    commit

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import '../App.css'
 import search from '../assets/cart.png'
 import cart from '../assets/search.png'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import amazon from '../assets/amazon.png'
 import greenbackground from '../assets/greenbackground.jpg'
 
