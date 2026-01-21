4cc3e3127141b678f81d23841356671a09c9264e
wprkftlgus <wprkftlgus@naver.com>
2026-01-21 17:17:46 +0900

    commit

--- server/routes/login.js
+++ server/routes/login.js
@@ -34,7 +34,7 @@ loginRouter.post("/login", async (req,res) => {
       res.cookie("token", token, {
       httpOnly: true, 
       secure: false, 
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, 
     });
       res.json({ message: "Login successful"})
