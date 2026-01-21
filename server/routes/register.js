ecc97a78dffd44a18733d9735e0ec667f6ba071e
wprkftlgus <wprkftlgus@naver.com>
2026-01-21 16:52:44 +0900

    commit

--- server/routes/register.js
+++ server/routes/register.js
@@ -31,6 +31,7 @@ RegisterRouter.post('/register', async (req,res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000
         });
 
